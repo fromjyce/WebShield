d8581d5181c6ed5c91cdf1492892227b575b945d
jaya2 <jaya2004kra@gmail.com>
2024-08-17 09:41:18 +0530

    added phishsearch

---
+++ webshield-website/src/components/PhishSearch.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import { Container } from "react-bootstrap";
+import "../styles/PhishSearch.css"; // Import custom CSS for additional styling
+
+function PhishSearch() {
+  return (
+    <Container>
+      <h2 className="recent-submissions-heading">Recent Submissions</h2>
+      {/* Add additional content here */}
+    </Container>
+  );
+}
+
+export default PhishSearch;
