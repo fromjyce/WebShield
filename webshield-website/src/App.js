6596c128c1ea54ae08bae4183d1a1eebc8efc735
jaya2 <jaya2004kra@gmail.com>
2024-08-17 09:42:25 +0530

    Added routers

--- webshield-website/src/App.js
+++ webshield-website/src/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavbarComponent from "./components/NavbarComponent";
 import BodyComponent from "./components/BodyComponent";
+import PhishSearch from "./components/PhishSearch";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <NavbarComponent />
-      <BodyComponent />
-    </div>
+      <Routes>
+        <Route path="/" element={<BodyComponent />} />
+        <Route path="/phishsearch" element={<PhishSearch />} />
+      </Routes>
+    </Router>
   );
 }
 
